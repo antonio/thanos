b8df8ba161f771e1899cdad52b0b4415cbed01a4
Antonio Santos <antonio@santosvelasco.com>
2019-10-22 12:48:43 +0200

    Read the access token from the config

--- pkg/tracing/lightstep/lightstep.go
+++ pkg/tracing/lightstep/lightstep.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-kit/kit/log"
 	"github.com/lightstep/lightstep-tracer-go"
 	"github.com/opentracing/opentracing-go"
+	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
@@ -21,9 +22,14 @@ type Tracer struct {
 func (t *Tracer) Close() {
 }
 
-func NewTracer(ctx context.Context, logger log.Logger, conContentYaml []byte) (opentracing.Tracer, io.Closer, error) {
+func NewTracer(ctx context.Context, logger log.Logger, yamlConfig []byte) (opentracing.Tracer, io.Closer, error) {
+	config := Config{}
+	if err := yaml.Unmarshal(yamlConfig, &config); err != nil {
+		return nil, nil, err
+	}
+
 	options := lightstep.Options{
-		AccessToken: "PLACEHOLDER", // TODO: Fix this
+		AccessToken: config.AccessToken,
 	}
 	tracer := lightstep.NewTracer(options)
 	if tracer == nil { // lightstep.NewTracer returns nil when there is an error
